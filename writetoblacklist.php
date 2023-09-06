60ec72b2cd1b1ba1521d20e2df4df0acfa7249ba
AirRhyme <84876147+AirRhyme@users.noreply.github.com>
2023-09-06 12:07:58 -0400

    Create writetoblacklist.php

---
+++ writetoblacklist.php
@@ -0,0 +1,20 @@
+<?php
+$file = 'blacklist.json';
+$userId = $_POST['userId'];
+$reason = $_POST['reason'];
+
+$blacklistData = file_get_contents($file);
+$blacklistArray = json_decode($blacklistData, true);
+
+error_log($userId)
+error_log($reason)
+
+$blacklistArray[$userId] = $reason;
+$newBlacklistData = json_encode($blacklistArray);
+
+if (file_put_contents($file, $newBlacklistData)) {
+    echo "Blacklist entry added successfully";
+} else {
+    echo "Error adding blacklist entry";
+}
+?>
